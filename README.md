f960a973ac5f2d1a5b25c03485dff6e0171abeb3
jarheadSLO <klemen1111@gmail.com>
2017-05-20 15:00:21 +0200

    Update Prerequisites section in readme

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Prezentio
 Control your presentation with alternative methods
 
-## Prerequisites
+## Prerequisites and preparation development environment
 ```bash
 install python-opencv, pygame, numpy, scipy, simplecv
+```
+
+Go to [http://www.lfd.uci.edu/~gohlke/pythonlibs/#opencv](http://www.lfd.uci.edu/~gohlke/pythonlibs/#opencv) and download opencv prebuilt whl for your version of python.
+Then run:
+```bash
+pip install <name-of-whl>.whl
 ```
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from SimpleCV import Image, Camera
+import cv2
 
 if __name__ == '__main__':
     print("Start")
 
-    camera = Camera()
-
+    camera = cv2.VideoCapture(0)
+    while True:
+        return_value, image = camera.read()
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('image', gray)
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            cv2.imwrite('test.jpg', image)
+            break
+    camera.release()
+    cv2.destroyAllWindows()
 
     print("End")
