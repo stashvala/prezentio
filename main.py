cf8d7d26425ca08c75af6cd419363edb5154bfd4
Stas Hvala <stas.hvala@gmail.com>
2017-05-21 04:18:48 +0200

    First working version

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2
 import time
+import collections
+import pyautogui as pag
 
 if __name__ == '__main__':
     print("Start")
@@ -14,27 +16,34 @@ if __name__ == '__main__':
 
     start = 0
     history = []
+    detected = 0
+
+    face_queue = collections.deque(maxlen=10)
 
     while True:
         return_value, image = camera.read()
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         hands = hand_cascade.detectMultiScale(gray, 1.3, 5)
-        face = face_cascade.detectMultiScale(gray, 1.3, 5)
 
         if len(hands) == 0:
+            if detected == 1:
+                print("right")
+                pag.press('right')
+            elif detected == 2:
+                print("left")
+                pag.press('left')
+
+            detected = 0
             start = 0
             history = []
 
         for (x, y, w, h) in hands:
             cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-            #print("before if = ", start)
             if start == 0:
                 start = time.time()
-                #print("start = ", start)
 
-            print((time.time() - start))
             if (time.time() - start) > time_thresh and len(history) > hist_thresh:
                 avgx = 0
                 avgy = 0
@@ -43,21 +52,28 @@ if __name__ == '__main__':
                     avgy += i[1]
                 avgx /= len(history)
                 avgy /= len(history)
-                stddevx = 0
-                stddevy = 0
-                for i in history:
-                    stddevx += (avgx - i[0]) ^ 2
-                    stddevy += (avgy - i[1]) ^ 2
-                if stddevx < 2 and stddevy < 2:
-                    print("Success")
-                    history = 0
-                    start = 0
+
+                avgfacex = 0
+                avgfacey = 0
+                for i in face_queue:
+                    avgfacex += i[0]
+                    avgfacey += i[1]
+
+                if detected == 0:
+                    if avgx < avgfacex:
+                        detected = 1
+                        print("r")
+                    else:
+                        detected = 2
+                        print("l")
 
             else:
                 history.append([x, y])
 
-                # for (x2, y2, w2, h2) in face:
-                # cv2.rectangle(image, (x2, y2), (x2 + w2, y2 + h2), (0, 255, 0), 2)
+        face = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x2, y2, w2, h2) in face:
+            cv2.rectangle(image, (x2, y2), (x2 + w2, y2 + h2), (0, 255, 0), 2)
+            face_queue.append([x2, y2])
 
         cv2.imshow('image', image)
         if cv2.waitKey(1) & 0xFF == ord('s'):
