c95b5ddd6056fb21a4387053526172a92193f509
Stas Hvala <stas.hvala@gmail.com>
2017-05-21 03:39:13 +0200

    Detect hand and validate

--- main.py
+++ main.py
@@ -1,102 +1,69 @@
-'''import cv2
-import numpy as np
+import cv2
 import time
-
-blur_size = 3
 
 if __name__ == '__main__':
     print("Start")
 
+    hand_cascade = cv2.CascadeClassifier('hand.xml')
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+
     camera = cv2.VideoCapture(0)
+
+    time_thresh = 1
+    hist_thresh = 10
+
+    start = 0
+    history = []
 
     while True:
         return_value, image = camera.read()
-        image = cv2.blur(image, (blur_size, blur_size))
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        image = cv2.inRange(image, np.array([2, 50, 50]), np.array([15, 255, 255]))
+        hands = hand_cascade.detectMultiScale(gray, 1.3, 5)
+        face = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        if len(hands) == 0:
+            start = 0
+            history = []
+
+        for (x, y, w, h) in hands:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+            #print("before if = ", start)
+            if start == 0:
+                start = time.time()
+                #print("start = ", start)
+
+            print((time.time() - start))
+            if (time.time() - start) > time_thresh and len(history) > hist_thresh:
+                avgx = 0
+                avgy = 0
+                for i in history:
+                    avgx += i[0]
+                    avgy += i[1]
+                avgx /= len(history)
+                avgy /= len(history)
+                stddevx = 0
+                stddevy = 0
+                for i in history:
+                    stddevx += (avgx - i[0]) ^ 2
+                    stddevy += (avgy - i[1]) ^ 2
+                if stddevx < 2 and stddevy < 2:
+                    print("Success")
+                    history = 0
+                    start = 0
+
+            else:
+                history.append([x, y])
+
+                # for (x2, y2, w2, h2) in face:
+                # cv2.rectangle(image, (x2, y2), (x2 + w2, y2 + h2), (0, 255, 0), 2)
 
         cv2.imshow('image', image)
         if cv2.waitKey(1) & 0xFF == ord('s'):
             break
+
     camera.release()
     cv2.destroyAllWindows()
 
     print("End")
-'''
-
-'''
-import cv2
-import numpy as np
-import math
-
-cap = cv2.VideoCapture(0)
-while (cap.isOpened()):
-    ret, img = cap.read()
-    cv2.rectangle(img, (300, 300), (100, 100), (0, 255, 0), 0)
-    crop_img = img[100:300, 100:300]
-    grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-    value = (35, 35)
-    blurred = cv2.GaussianBlur(grey, value, 0)
-    _, thresh1 = cv2.threshold(blurred, 127, 255,
-                               cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    cv2.imshow('Thresholded', thresh1)
-
-    (version, _, _) = cv2.__version__.split('.')
-
-    if version is '3':
-        image, contours, hierarchy = cv2.findContours(thresh1.copy(), \
-                                                      cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    elif version is '2':
-        contours, hierarchy = cv2.findContours(thresh1.copy(), cv2.RETR_TREE, \
-                                               cv2.CHAIN_APPROX_NONE)
-
-    cnt = max(contours, key=lambda x: cv2.contourArea(x))
-
-    x, y, w, h = cv2.boundingRect(cnt)
-    cv2.rectangle(crop_img, (x, y), (x + w, y + h), (0, 0, 255), 0)
-    hull = cv2.convexHull(cnt)
-    drawing = np.zeros(crop_img.shape, np.uint8)
-    cv2.drawContours(drawing, [cnt], 0, (0, 255, 0), 0)
-    cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 0)
-    hull = cv2.convexHull(cnt, returnPoints=False)
-    defects = cv2.convexityDefects(cnt, hull)
-    count_defects = 0
-    cv2.drawContours(thresh1, contours, -1, (0, 255, 0), 3)
-    if defects is not None:
-        for i in range(defects.shape[0]):
-            s, e, f, d = defects[i, 0]
-            start = tuple(cnt[s][0])
-            end = tuple(cnt[e][0])
-            far = tuple(cnt[f][0])
-            a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
-            b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
-            c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
-            angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)) * 57
-            if angle <= 90:
-                count_defects += 1
-                cv2.circle(crop_img, far, 1, [0, 0, 255], -1)
-            # dist = cv2.pointPolygonTest(cnt,far,True)
-            cv2.line(crop_img, start, end, [0, 255, 0], 2)
-            # cv2.circle(crop_img,far,5,[0,0,255],-1)
-        if count_defects == 1:
-            cv2.putText(img, "I am Vipul", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-        elif count_defects == 2:
-            str = "This is a basic hand gesture recognizer"
-            cv2.putText(img, str, (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
-        elif count_defects == 3:
-            cv2.putText(img, "This is 4 :P", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-        elif count_defects == 4:
-            cv2.putText(img, "Hi!!!", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-        else:
-            cv2.putText(img, "Hello World!!!", (50, 50), \
-                        cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-    # cv2.imshow('drawing', drawing)
-    # cv2.imshow('end', crop_img)
-    cv2.imshow('Gesture', img)
-    all_img = np.hstack((drawing, crop_img))
-    cv2.imshow('Contours', all_img)
-    k = cv2.waitKey(10)
-    if k == 27:
-        break
-'''
